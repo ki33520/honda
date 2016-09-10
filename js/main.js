058343035533137aca84a6d44dd5546dd76ecd6b
ki33520 <ki33520@gmail.com>
2016-09-10 18:06:44 +0800

    start

---
+++ js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+var globel = {
+
+};
+
+var bodySwiper = new Swiper('.swiper-container', {
+	pagination: '.swiper-pagination',
+	paginationClickable: true,
+	direction: 'vertical'
+});
